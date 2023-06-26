ba696ea82ce8eb7377592f455cd01684c556569a
RemainingToast <Remainingtoast@gmail.com>
2023-06-26 23:43:56 +1000

    fix id

--- src/main/java/net/austechmc/discord/util/Constants.java
+++ src/main/java/net/austechmc/discord/util/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final Emoji FIRE = Emoji.fromUnicode("U+1F525");
     public static final Emoji TRASH = Emoji.fromUnicode("U+1F5D1");
 
-    public static final long BOT_CHANNEL_ID = 1122877462347513996L;
+    public static final long BOT_CHANNEL_ID = 1122884309804646410L;
     public static final long MEDIA_CHANNEL_ID = 1121311387055050823L;
     public static final long APPLICATION_CHANNEL_ID = 1105434379804754030L;
     public static final long MEMBER_ROLE_ID = 1105435087601934420L;
